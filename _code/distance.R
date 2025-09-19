86c173081b0909a88f79fafb1776ae5a84ae112c
Shannon Rankin <59101864+shannonrankin@users.noreply.github.com>
2025-09-19 09:51:19 -0700

    update
    update

--- _code/distance.R
+++ _code/distance.R
@@ -8,3 +8,27 @@ library(here)
 here()
 
 source(here::here("_code/_common.R"))
+
+
+#############
+# data prep #
+#############
+# mydistanceData <- "DDtest_20250717_030700.WAV.Table.1.selections.txt" #give name of the distance dataset
+# distdata <- read_tsv(here("data", mydistanceData))  # my data, must have distance, ID, and then add group size
+# glimpse(distdata)
+
+birdnet_SPP_LFRR <- read_csv(here("output", "birdnet_SPP_LFRR.csv"))
+
+
+
+  
+######################
+# detection function #
+######################
+#https://workshops.distancesampling.org/duke-spatial-2015/practicals/1-detection-functions-solutions.html
+
+df_hn <- ds(data=distdata, truncation=1000, key="hn", adjustment=NULL)
+summary(df_hn)
+
+ddf.gof(df_hn$ddf, asp=1)
+plot(df_hn)
